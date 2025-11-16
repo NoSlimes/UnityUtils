7ca3f5b2fa698e57f5d85376d45cf127088a0ce9
NoSlimes <elias.wennerberg@gmail.com>
2025-11-16 21:09:41 +0100

    Adds InputManager and console handler
    Introduces InputManager for centralized input handling with
    action mapping and callbacks. Includes console handler for
    developer console integration.
    
    Updates assembly definition references for new components.

---
+++ Runtime/Input/InputManager.cs
@@ -0,0 +1,258 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace NoSlimes.Utils.Input
+{
+    public struct ActionNames
+    {
+        public const string Move = "Move";
+        public const string Jump = "Jump";
+        public const string Crouch = "Crouch";
+        public const string Sprint = "Sprint";
+        public const string Look = "Look";
+        public const string Attack = "Attack";
+        public const string Interact = "Interact";
+        public const string HotbarScroll = "HotbarScroll";
+        public const string HotbarSelect = "HotbarSelect";
+    }
+
+    [DefaultExecutionOrder(-10)]
+    [RequireComponent(typeof(PlayerInput))]
+    public class InputManager : MonoBehaviour
+    {
+        private static InputManager _instance;
+        public static InputManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = FindAnyObjectByType<InputManager>();
+
+                if (_instance == null)
+                {
+                    return null;
+                }
+
+                return _instance;
+            }
+        }
+
+        private PlayerInput playerInput;
+        private string previousActionMapName;
+
+        private readonly Dictionary<string, InputAction> inputActions = new();
+        private readonly Dictionary<string, Action<InputAction.CallbackContext>> performedCallbacks = new();
+        private readonly Dictionary<string, Action<InputAction.CallbackContext>> startedCallbacks = new();
+        private readonly Dictionary<string, Action<InputAction.CallbackContext>> canceledCallbacks = new();
+
+        public PlayerInput PlayerInput => playerInput;
+        public bool IsGamepad => PlayerInput.currentControlScheme == "Gamepad";
+        public InputActionMap PreviousActionMap => playerInput.actions.FindActionMap(previousActionMapName);
+
+        // Optional centralized events
+        public event Action<InputAction.CallbackContext> OnMove;
+        public event Action<InputAction.CallbackContext> OnJump;
+        public event Action<InputAction.CallbackContext> OnCrouch;
+        public event Action<InputAction.CallbackContext> OnSprint;
+        public event Action<InputAction.CallbackContext> OnLook;
+        public event Action<InputAction.CallbackContext> OnAttack;
+        public event Action<InputAction.CallbackContext> OnInteract;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+
+            playerInput = GetComponent<PlayerInput>();
+            if (playerInput == null)
+            {
+                Debug.LogError("PlayerInput component is missing on the GameObject.", this);
+                return;
+            }
+
+            playerInput.notificationBehavior = PlayerNotifications.InvokeCSharpEvents;
+
+            CacheActions();
+        }
+
+        private void OnEnable()
+        {
+            playerInput.onActionTriggered += HandleActionTriggered;
+            playerInput.onControlsChanged += OnInputControlsChanged;
+
+        }
+
+        private void OnDisable()
+        {
+            if (playerInput != null)
+            {
+                playerInput.onActionTriggered -= HandleActionTriggered;
+                playerInput.onControlsChanged -= OnInputControlsChanged;
+            }
+
+        }
+
+        private void OnInputControlsChanged(PlayerInput obj)
+        {
+            CacheActions();
+        }
+
+        private void CacheActions()
+        {
+            inputActions.Clear();
+            foreach (InputAction action in playerInput.actions)
+            {
+                inputActions[action.name] = action;
+            }
+        }
+
+        private void HandleActionTriggered(InputAction.CallbackContext context)
+        {
+            string name = context.action.name;
+
+            // Centralized events (optional)
+            switch (name)
+            {
+                case ActionNames.Move: OnMove?.Invoke(context); break;
+                case ActionNames.Jump: OnJump?.Invoke(context); break;
+                case ActionNames.Crouch: OnCrouch?.Invoke(context); break;
+                case ActionNames.Sprint: OnSprint?.Invoke(context); break;
+                case ActionNames.Look: OnLook?.Invoke(context); break;
+                case ActionNames.Attack: OnAttack?.Invoke(context); break;
+                case ActionNames.Interact: OnInteract?.Invoke(context); break;
+            }
+
+            // Phase-specific callbacks
+            if (context.started && startedCallbacks.TryGetValue(name, out Action<InputAction.CallbackContext> started))
+                started?.Invoke(context);
+
+            if (context.performed && performedCallbacks.TryGetValue(name, out Action<InputAction.CallbackContext> performed))
+                performed?.Invoke(context);
+
+            if (context.canceled && canceledCallbacks.TryGetValue(name, out Action<InputAction.CallbackContext> canceled))
+                canceled?.Invoke(context);
+        }
+
+        public InputAction GetAction(string name)
+        {
+            if (inputActions.TryGetValue(name, out InputAction action))
+                return action;
+
+            Debug.LogWarning($"Input action '{name}' not found in PlayerInput actions.", this);
+            return null;
+        }
+
+        public bool TryGetAction(string name, out InputAction action)
+        {
+            return inputActions.TryGetValue(name, out action);
+        }
+
+        public void RegisterActionCallback(string actionName, Action<InputAction.CallbackContext> callback, InputActionPhase phase = InputActionPhase.Performed)
+        {
+            if (string.IsNullOrEmpty(actionName) || callback == null)
+            {
+                Debug.LogWarning("Invalid action name or callback provided.", this);
+                return;
+            }
+
+            if (!inputActions.ContainsKey(actionName))
+            {
+                Debug.LogWarning($"Input action '{actionName}' not found. Cannot register callback.", this);
+                return;
+            }
+
+            switch (phase)
+            {
+                case InputActionPhase.Started:
+                    startedCallbacks.TryAdd(actionName, null);
+                    startedCallbacks[actionName] += callback;
+                    break;
+
+                case InputActionPhase.Performed:
+                    performedCallbacks.TryAdd(actionName, null);
+                    performedCallbacks[actionName] += callback;
+                    break;
+
+                case InputActionPhase.Canceled:
+                    canceledCallbacks.TryAdd(actionName, null);
+                    canceledCallbacks[actionName] += callback;
+                    break;
+
+                default:
+                    Debug.LogWarning("Unsupported input phase for registration.", this);
+                    break;
+            }
+        }
+
+        public void UnregisterActionCallback(string actionName, Action<InputAction.CallbackContext> callback, InputActionPhase phase = InputActionPhase.Performed)
+        {
+            if (string.IsNullOrEmpty(actionName) || callback == null)
+                return;
+
+            switch (phase)
+            {
+                case InputActionPhase.Started:
+                    if (startedCallbacks.TryGetValue(actionName, out Action<InputAction.CallbackContext> started))
+                    {
+                        started -= callback;
+                        if (started == null) startedCallbacks.Remove(actionName);
+                        else startedCallbacks[actionName] = started;
+                    }
+                    break;
+
+                case InputActionPhase.Performed:
+                    if (performedCallbacks.TryGetValue(actionName, out Action<InputAction.CallbackContext> performed))
+                    {
+                        performed -= callback;
+                        if (performed == null) performedCallbacks.Remove(actionName);
+                        else performedCallbacks[actionName] = performed;
+                    }
+                    break;
+
+                case InputActionPhase.Canceled:
+                    if (canceledCallbacks.TryGetValue(actionName, out Action<InputAction.CallbackContext> canceled))
+                    {
+                        canceled -= callback;
+                        if (canceled == null) canceledCallbacks.Remove(actionName);
+                        else canceledCallbacks[actionName] = canceled;
+                    }
+                    break;
+            }
+        }
+
+        public void SwitchActionMap(string mapName)
+        {
+            if (playerInput.currentActionMap.name != mapName)
+            {
+                previousActionMapName = playerInput.currentActionMap.name;
+
+                playerInput.SwitchCurrentActionMap(mapName);
+                Debug.Log($"Switched input map to '{mapName}'", this);
+                CacheActions();
+            }
+        }
+
+
+
+
+        #region Convenience Properties
+        public Vector2 Move => GetAction(ActionNames.Move)?.ReadValue<Vector2>() ?? Vector2.zero;
+
+        public Vector2 Look
+        {
+            get
+            {
+                Vector2 lookInput = GetAction(ActionNames.Look)?.ReadValue<Vector2>() ?? Vector2.zero;
+                return new Vector2(lookInput.x, -lookInput.y); // invert Y axis
+            }
+        }
+
+        public bool JumpPressed => GetAction(ActionNames.Jump)?.triggered ?? false;
+        public bool IsCrouching => GetAction(ActionNames.Crouch)?.triggered ?? false;
+        public bool IsSprinting => GetAction(ActionNames.Sprint)?.IsPressed() ?? false;
+        public bool IsAttacking => GetAction(ActionNames.Attack)?.triggered ?? false;
+        public bool IsInteracting => GetAction(ActionNames.Interact)?.IsPressed() ?? false;
+        #endregion
+    }
+}
---
+++ Runtime/Input/InputManagerConsoleHandler.cs
@@ -0,0 +1,32 @@
+#if DEVCON
+using NoSlimes.Util.DevCon;
+using UnityEngine;
+
+namespace NoSlimes.Utils.Input.Compatibility
+{
+    public class InputManagerConsoleHandler : MonoBehaviour
+    {
+        [SerializeField] private string uiActionMapName = "UI";
+
+        private InputManager inputManager;
+
+        private void OnEnable()
+        {
+            inputManager = InputManager.Instance;
+
+            DeveloperConsoleUI.OnConsoleToggled += HandleConsoleToggled;
+        }
+
+        private void OnDisable()
+        {
+            DeveloperConsoleUI.OnConsoleToggled -= HandleConsoleToggled;
+        }
+
+        private void HandleConsoleToggled(bool isActive)
+        {
+            if (inputManager == null || inputManager.PreviousActionMap == null) return;
+            inputManager.SwitchActionMap(isActive ? uiActionMapName : inputManager.PreviousActionMap.name);
+        }
+    }
+}
+#endif
