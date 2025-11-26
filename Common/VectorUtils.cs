683c5d63abe715d7d62a46f31d2839169655846e
NoSlimes <67907136+NoSlimes@users.noreply.github.com>
2025-11-26 13:32:42 +0100

    Adds Vector3 extension methods
    Adds extension methods to the Vector3 class for creating new vectors with modified X, Y, or Z components.
    
    These methods provide a more fluent and readable way to create new Vector3 instances based on an existing vector with only one component changed.

---
+++ Common/VectorUtils.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+namespace NoSlimes.Utils
+{
+    public static class VectorUtils
+    {
+        public static Vector3 WithX(this Vector3 vector, float newX)
+        {
+            return new Vector3(newX, vector.y, vector.z);
+        }
+
+        public static Vector3 WithY(this Vector3 vector, float newY)
+        {
+            return new Vector3(vector.x, newY, vector.z);
+        }
+
+        public static Vector3 WithZ(this Vector3 vector, float newZ)
+        {
+            return new Vector3(vector.x, vector.y, newZ);
+        }
+    }
+}
