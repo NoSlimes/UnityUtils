5581ad9eebb4dbe194482bcce2015b9049a52d94
NoSlimes <elias.wennerberg@gmail.com>
2025-12-01 20:36:43 +0100

    Improves read-only property display in editor.
    Ensures that read-only properties are displayed correctly in the editor, including correct height calculation and rendering of complex properties.
    
    The property field now respects the `property.isExpanded` state and displays all child properties when expanded.

--- Editor/AttributeDrawers/ReadOnlyFieldAttributeDrawer.cs
+++ Editor/AttributeDrawers/ReadOnlyFieldAttributeDrawer.cs
@@ -7,10 +7,16 @@ namespace NoSlimes.Utils.Editor.Drawers
     [CustomPropertyDrawer(typeof(ReadOnlyFieldAttribute))]
     internal class ReadOnlyFieldAttributeDrawer : PropertyDrawer
     {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             GUI.enabled = false;
-            EditorGUI.PropertyField(position, property, label);
+            EditorGUI.PropertyField(position, property, label, true);
+
             GUI.enabled = true;
         }
     }
