f2cf5e494cd0afbc1d79d3867f0a5e2cafe2e5bb
NoSlimes <elias.wennerberg@gmail.com>
2026-03-26 11:28:04 +0100

    Add support for NGO in ButtonMethods

--- Editor/Editors/ButtonEditor.cs
+++ Editor/Editors/ButtonEditor.cs
@@ -1,5 +1,7 @@
-using NoSlimes.UnityUtils.Common.Attributes;
+using System.Collections.Generic;
 using System.Reflection;
+using NoSlimes.UnityUtils.Common.Attributes;
+using NoSlimes.UnityUtils.Editor.Editors;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,22 +9,34 @@ namespace NoSlimes.UnityUtils.Editor.Editors
 {
     public static class ButtonDrawer
     {
-        public static void DrawButtons(object targetObject)
+        public static void DrawButtons(UnityEngine.Object[] targets)
         {
-            // Get all methods (Public, Private, Instance)
-            MethodInfo[] methods = targetObject.GetType()
-                .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            if (targets == null || targets.Length == 0) return;
 
-            foreach (MethodInfo method in methods)
+            var type = targets[0].GetType();
+            var methodGroups = new List<MethodInfo>();
+
+            var currentType = type;
+            while (currentType != null && currentType != typeof(MonoBehaviour) && currentType != typeof(ScriptableObject))
             {
-                var buttonAttr = method.GetCustomAttribute<ButtonMethodAttribute>();
-                if (buttonAttr != null)
-                {
-                    string label = string.IsNullOrEmpty(buttonAttr.Label) ? method.Name : buttonAttr.Label;
+                var methods = currentType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+                methodGroups.AddRange(methods);
+                currentType = currentType.BaseType;
+            }
 
-                    if (GUILayout.Button(label))
+            foreach (var method in methodGroups)
+            {
+                var attr = method.GetCustomAttribute<ButtonMethodAttribute>();
+                if (attr == null) continue;
+
+                string label = string.IsNullOrEmpty(attr.Label) ? method.Name : attr.Label;
+
+                if (GUILayout.Button(label))
+                {
+                    foreach (var t in targets)
                     {
-                        method.Invoke(targetObject, null);
+                        method.Invoke(t, null);
+                        EditorUtility.SetDirty(t); 
                     }
                 }
             }
@@ -36,7 +50,7 @@ namespace NoSlimes.UnityUtils.Editor.Editors
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
-            ButtonDrawer.DrawButtons(target);
+            ButtonDrawer.DrawButtons(targets);
         }
     }
 
@@ -47,7 +61,21 @@ namespace NoSlimes.UnityUtils.Editor.Editors
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
-            ButtonDrawer.DrawButtons(target);
+            ButtonDrawer.DrawButtons(targets);
         }
     }
+
+#if USE_NGO
+    [CustomEditor(typeof(Unity.Netcode.NetworkBehaviour), true)]
+    [CanEditMultipleObjects]
+    public class NetworkBehaviourButtonEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI(); // This draws the default NetworkBehaviour fields
+            ButtonDrawer.DrawButtons(targets);
+        }
+    }
+#endif
+
 }
